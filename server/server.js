9795cb5e260763a7e5f30b4b0f8ea4a0971915e2
Mentor Aliu <mentor.aaliu@outlook.com>
2022-01-23 12:40:10 +0100

    completed backed user authentication

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 const app = express();
 
 app.use(express.json());
 dotenv.config();
-
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -16,5 +16,6 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => console.log(`Server running on port 8080`));
